32f2a01f81760fb1d7daa640add7c2f66b8201d4
Naveen757kumar <123856105+Naveen757kumar@users.noreply.github.com>
2024-01-23 17:49:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Register from './Components/Register';
+import Login from './Components/Login.js'
+import PasswordReset from './Components/PasswordReset';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Taskpage from './Components/Taskpage.js';
+import Taskupdate from './Components/Taskupdate';
+import Taskform from './Components/Taskform.js';
+import TaskDetails from './Components/TaskDetails.js';
+import React from 'react';
 
 function App() {
+  const MyComponent = React.lazy(() => import('./Components/Login.js'));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register/>} />
+        <Route path="/forgot-password" element={<PasswordReset/>}/>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/welcome" element={<Taskpage/>}></Route>
+        <Route path="/update" element={<Taskupdate/>}></Route>
+        <Route path="/task" element={<TaskDetails />} />
+        <Route path="/form" element={<Taskform/>}></Route>
+        <Route path="/" component={MyComponent} />
+
+      </Routes>
+    </Router>
+    
   );
 }
 
